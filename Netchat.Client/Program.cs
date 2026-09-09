068d2ba83a87d2605245a8e0e4ef114fc5aa9239
Elliot Antoun <elliotabouantoun@gmail.com>
2026-09-09 16:37:32 -0400

    In Netchat.Client, added server retry logic in case server is not running. Discards the initial tcp client object and creates a new one to avoid problems.

--- Netchat.Client/Program.cs
+++ Netchat.Client/Program.cs
@@ -5,12 +5,29 @@
  * Function: Connect to server on localhost and port.
  */
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+bool isConnected = false;
 
 TcpClient client = new TcpClient();
 
-Console.WriteLine("Connecting to Server...");
 
-await client.ConnectAsync("127.0.0.1", 5000);
+while(!isConnected)
+{
+    try
+    {
+        Console.WriteLine("Connecting to Server...");
+        await client.ConnectAsync("127.0.0.1", 5000);
+        isConnected = true;
+    }
+    catch (SocketException ex)
+    {
+        Console.WriteLine("Server Unavailable");
+        Console.WriteLine("Retrying in 5 seconds...");
+        await Task.Delay(5000);
+        client = new TcpClient();
+        }
+    }
 
 Console.WriteLine("Successfully connected to server on port 5000!");
 
