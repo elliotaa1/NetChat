8ba123fae9eb9c4051cbd4f32f10d27035e8d6a8
Elliot Antoun <elliotabouantoun@gmail.com>
2026-09-09 16:20:45 -0400

    Created TcpListener and TcpClient.

--- Netchat.Server/Program.cs
+++ Netchat.Server/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+
+TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+
+listener.Start();
+
+Console.WriteLine("NetChat Server started.");
+Console.WriteLine("Listening on port 5000...");
+Console.WriteLine("Waiting for clients to connect...");
+
+TcpClient client = await listener.AcceptTcpClientAsync();
+
+Console.WriteLine("Client connected!");
+
+Console.ReadLine();
