6034321c7cda071316f39f90d57ddb7d85312bdc
Elliot Antoun <elliotabouantoun@gmail.com>
2026-09-09 18:05:10 -0400

    Another big commit before break. Added client and server-side object cleanup. Allows multiple connections to the server. User can disconnect from session and restart, server awaits new connection. Implemented additional logic validation.

--- Netchat.Client/Program.cs
+++ Netchat.Client/Program.cs
@@ -6,69 +6,84 @@
  */
 using System.Net.Sockets;
 using System.Text;
+string? restartInput;
 
-bool isConnected = false;
-bool isMsg = false;
-
-TcpClient client = new TcpClient();
-
-while (!isConnected)
+do
 {
-    try
-    {
-        Console.WriteLine("Connecting to Server...");
-        await client.ConnectAsync("127.0.0.1", 5000);
-        isConnected = true;
-        Console.WriteLine("Successfully connected to server on port 5000!");
-    }
-    catch (SocketException ex)
-    {
-        Console.WriteLine("Server Unavailable");
-        Console.WriteLine("Retrying in 5 seconds...");
-        await Task.Delay(5000);
-        client = new TcpClient();
-        }
-    }
-NetworkStream stream = client.GetStream();
+        bool isConnected = false;
+        bool isMsg = false;
 
-Console.WriteLine("Welcome to NetChat Client!\nPlease enter your name: ");
-string? userName = Console.ReadLine();
+        TcpClient client = new TcpClient();
 
-if(!string.IsNullOrWhiteSpace(userName))
+        while (!isConnected)
+        {
+            try
+            {
+                Console.WriteLine("Connecting to Server...");
+                await client.ConnectAsync("127.0.0.1", 5000);
+                isConnected = true;
+                Console.WriteLine("Successfully connected to server on port 5000!");
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Server Unavailable");
+                Console.WriteLine("Retrying in 5 seconds...");
+                await Task.Delay(5000);
+                client = new TcpClient();
+                }
+            }
+
+        NetworkStream stream = client.GetStream();
+
+        Console.WriteLine("Welcome to NetChat Client!\nPlease enter your name: ");
+        string? userName = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(userName))
+            {
+                Console.WriteLine($"Hello {userName}! You can now send messages to the server.");
+
+                byte[] username = Encoding.UTF8.GetBytes(userName);
+                await stream.WriteAsync(username);
+            }
+
+
+            while (!isMsg)
+            {
+                Console.Write($"{userName}: ");
+                string? userInput = Console.ReadLine();
+                isMsg = true;
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("No message entered.");
+                    continue;
+                }
+
+                if (userInput.ToLower() == "exit")
+                {
+                    Console.WriteLine("Exiting session...");
+                    break;
+                }
+                byte[] data = Encoding.UTF8.GetBytes(userInput);
+                await stream.WriteAsync(data);
+                //Console.WriteLine("Sent message to server!");
+                isMsg = false;
+            }
+
+        stream.Dispose();
+        client.Dispose();
+        Console.Write("Restore Client Session? Y/N: ");
+            restartInput = Console.ReadLine();
+
+} while (restartInput?.ToUpper() == "Y");
+
+
+if (restartInput?.ToUpper() == "N")
 {
-    Console.WriteLine($"Hello {userName}! You can now send messages to the server.");
-
-    byte[] username = Encoding.UTF8.GetBytes(userName);
-    await stream.WriteAsync(username);
+    Console.WriteLine("Exiting NetChat Client...");
+    return;
+    
 }
-
-
-while (!isMsg){
-    Console.Write($"{userName}: ");
-    string? userInput = Console.ReadLine();
-    isMsg = true;
-    if (!string.IsNullOrWhiteSpace(userInput))
-    {
-        byte[] data = Encoding.UTF8.GetBytes(userInput);
-        await stream.WriteAsync(data);
-        //Console.WriteLine("Sent message to server!");
-        isMsg = false;
-    }
-    else
-    {
-        Console.WriteLine("No message entered. Exiting...");
-    }
-}
-
 
 Console.ReadLine();
 
 
-
-//byte[] buffer = new byte[1024];
-
-//int bytesRead = await stream.ReadAsync(buffer);
-
-//string serverReplyMsg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-
-//Console.WriteLine($"Received message from Server: {serverReplyMsg}");
--- Netchat.Server/Program.cs
+++ Netchat.Server/Program.cs
@@ -13,28 +13,42 @@ Console.WriteLine("NetChat Server started.");
 Console.WriteLine("Listening on port 5000...");
 Console.WriteLine("Waiting for clients to connect...");
 
-TcpClient client = await listener.AcceptTcpClientAsync();
-
-Console.WriteLine("Client connected!");
-
-NetworkStream stream = client.GetStream();
-
-byte[] buffer = new byte[1024];
-
-byte[] usernameBuffer = new byte[1024];
-
-int bytesUsername = await stream.ReadAsync(usernameBuffer);
-
-string username = Encoding.UTF8.GetString(usernameBuffer, 0, bytesUsername);
 
 while (true)
 {
-    int bytesRead = await stream.ReadAsync(buffer);
+    TcpClient client = await listener.AcceptTcpClientAsync();
 
-    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    Console.WriteLine("Client connected!");
 
-    Console.WriteLine($"Received message from {username}: {message}");
+    NetworkStream stream = client.GetStream();
 
+
+    byte[] buffer = new byte[1024];
+
+    byte[] usernameBuffer = new byte[1024];
+
+    int bytesUsername = await stream.ReadAsync(usernameBuffer);
+
+    string username = Encoding.UTF8.GetString(usernameBuffer, 0, bytesUsername);
+
+    while (true)
+    {
+
+        int bytesRead = await stream.ReadAsync(buffer);
+
+        if (bytesRead == 0)
+        {
+            Console.WriteLine($"Client {username} disconnected.");
+            break;
+        }
+
+        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        Console.WriteLine($"Received message from {username}: {message}");
+
+    }
+
+    client.Dispose();
+    stream.Dispose();
+    Console.WriteLine("Awaiting new client connections...");
 }
-
-Console.ReadLine();
